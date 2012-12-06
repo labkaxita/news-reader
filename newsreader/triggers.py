cda37dc9062b6fb7e142e3daeefbedc94fa708e9
Unai Zalakain <contact@unaizalakain.info>
2012-12-06 20:16:08 +0100

    And and Or triggers

--- newsreader/triggers.py
+++ newsreader/triggers.py
@@ -4,6 +4,24 @@ class Trigger(object):
         raise NotImplemented()
 
 
+class And(objects):
+    def __init__(self, subtriggers=[]):
+        self.subtriggers = subtriggers
+
+    def is_activated(self, entry):
+        return all(( trigger.is_activated(entry) for trigger in \
+                self.subtriggers ))
+
+
+class Or(objects):
+    def __init__(self, subtriggers=[]):
+        self.subtriggers = subtriggers
+
+    def is_activated(self, entry):
+        return any(( trigger.is_activated(entry) for trigger in \
+                self.subtriggers ))
+
+
 class Contains(Trigger):
     def __init__(self, string, case_sensitive=False):
         self.string = string
