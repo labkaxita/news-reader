4dc797761e68d44daa830740caed02247e2519ed
Unai Zalakain <contact@unaizalakain.info>
2012-12-10 18:48:28 +0100

    main process method to keep on processing constantly

--- newsreader/__init__.py
+++ newsreader/__init__.py
@@ -1,1 +1,11 @@
-#
+registry = []
+
+def register(processor):
+    if processor not in registry:
+        registry.append(processor)
+
+
+def main():
+    while True:
+        for processor in registry:
+            processor.process()
