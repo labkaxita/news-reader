9987958a242593d88e77f72d126b2d77aaedf239
Unai Zalakain <contact@unaizalakain.info>
2012-12-13 01:52:57 +0100

    add example unicode formatter

--- newsreader/formatters.py
+++ newsreader/formatters.py
@@ -5,6 +5,12 @@ class Formatter(object):
         return entries
 
 
+class Unicode(Formatter):
+    def format(self, entries):
+        for entry in entries:
+            yield unicode(entry)
+
+
 class FeedDigest(Formatter):
     def format(self, entries):
         mail = ''
