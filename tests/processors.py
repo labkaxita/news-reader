aaad22a69632224db9b6e44086934e2e29c7b80c
Unai Zalakain <contact@unaizalakain.info>
2012-12-13 03:25:37 +0100

    updated processor tests

--- tests/processors.py
+++ tests/processors.py
@@ -1,6 +1,7 @@
 import unittest
 import random
 
+from newsreader.entry import Entry
 from newsreader.sources import RandomInt
 from newsreader.triggers import Random
 from newsreader.formatters import Unicode
@@ -20,7 +21,7 @@ class SourceProcessorTestCase(unittest.TestCase):
     def test_source_processor(self):
         random.seed(0)
         entries = self.source_processor.process()
-        results = [9, 8, 4, 2, 5, 4, 8, 3, 5, 6]
+        results = [ {'int': r} for r in [9, 8, 4, 2, 5, 4, 8, 3, 5, 6] ]
         self.assertEqual(list(entries), results)
 
 
@@ -51,35 +52,29 @@ class TriggerProcessorTestCase(unittest.TestCase):
 
 class HandlerProcessorTestCase(unittest.TestCase):
     def setUp(self):
-        self.entries = range(10)
+        self.entries = list(SourceProcessor({RandomInt(10, 0, 10): None}).process())
         self.handler_processor = HandlerProcessor({})
 
     def test_single_handler(self):
-        self.handler_processor.handlers = {Handler(): None}
+        self.handler_processor.handlers = [Handler()]
         results = self.handler_processor.process(self.entries)
         handler, results = results.next()
         self.assertEqual(list(results), self.entries)
 
     def test_two_handlers(self):
-        self.handler_processor.handlers = {Handler(): None, Handler(): None}
+        self.handler_processor.handlers = [Handler(), Handler()]
         results = self.handler_processor.process(self.entries)
         handler_0, results_0 = results.next()
         handler_1, results_1 = results.next()
         self.assertEqual(list(results_0), list(results_1), self.entries)
 
-    def test_handler_with_formatter(self):
-        self.handler_processor.handlers = {Handler(): Unicode()}
-        results = self.handler_processor.process(self.entries)
-        handler, results = results.next()
-        expected = [ unicode(entry) for entry in self.entries ]
-        self.assertEqual(list(results), expected)
 
 
 class ProcessorTestCase(unittest.TestCase):
     def setUp(self):
         self.sources = {RandomInt(10, 0, 10): None}
         self.triggers = [Random()]
-        self.handlers = {Handler(): Unicode()}
+        self.handlers = [Handler()]
 
     def test_empty_processor(self):
         processor = Processor()
@@ -90,6 +85,8 @@ class ProcessorTestCase(unittest.TestCase):
         random.seed(0)
         processor = Processor(self.sources, self.triggers, self.handlers)
         results = processor.main()
-        self.assertEqual(results.keys(), self.handlers.keys())
-        handler_results = results[self.handlers.keys()[0]]
-        self.assertEqual(handler_results, ['4', '5', '8', '6'])
+
+        self.assertEqual(results.keys(), self.handlers)
+        results = results.values()[0]
+        expected = [ {'int': r} for r in [4, 5, 8, 6] ]
+        self.assertEqual(results, expected)
