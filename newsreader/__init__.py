fa01366dc1dcd3b81b58a31cec7311cc094b1ef5
Unai Zalakain <contact@unaizalakain.info>
2012-12-13 14:11:50 +0100

    multiprocessing support for Processors

--- newsreader/__init__.py
+++ newsreader/__init__.py
@@ -1,3 +1,7 @@
+from itertool import repeat
+from multiprocessing import Pool
+
+
 registry = []
 
 def register(processor):
@@ -9,3 +13,16 @@ def main():
     while True:
         for processor in registry:
             processor.main()
+
+
+def async_main():
+    pool = Pool()
+    while True:
+        for processor in registry:
+            iterator = processor.process()
+            func = lambda x: iterator.next()
+            always = repeat(True)
+
+            process = pool.imap_unordered(func, always, chunksize=2)
+            results = { handler: list(results) for handler, results in process }
+            yield results
